c7cf29ad76695f1fa485caf6a88ee0451cf414f5
Man Leung <lepusleung@gmail.com>
2018-06-19 20:38:48 +0000

    Old code that is not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+
+  get 'songs/show'
+
+  get 'songs/index'
+
+  get 'songs/show'
+
+  get 'artists/index'
+
+  get 'artists/show'
+
   resources :artists, :songs
 end
